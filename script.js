5b7adb0af58c489ab82c3d468a807fe234850a33
animesh3640 <127398605+animesh3640@users.noreply.github.com>
2023-05-01 19:41:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+var firstName = "John";
+var lastName = "Mark Doe";
+var firstName, lastName, fullName;
+firstName = 'John';
+lastName = 'Mark Doe';
+fullName = 'firstName' + 'lastName';
 }
